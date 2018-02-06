a80fa19f05997e3ba0fe74d21b98d2ebea4eb355
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-06 16:36:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,15 @@ class Transfer
     end
   end
   def execute_transaction
-    if @status == "pending"  && @sender.balance >= @amount 
+    if @status == "pending"  && @sender.balance >= @amount
       @sender.balance -= @amount;
       @receiver.balance += @amount
       @status= "complete"
-    else 
-      @status = "rejected" #this also rejects the case that status is not pending. 
-      
+    else
+      @status = "rejected" #this also rejects the case that status is not pending.
+    end 
+  end 
+
 
   end
 
